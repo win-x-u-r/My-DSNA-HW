7e6cd084005d8e21bb14645d712fc4a474d61851
win-x-u-r <134261446+win-x-u-r@users.noreply.github.com>
2024-10-24 22:38:08 +0400

    2nd commit

--- Activities/HW.cpp
+++ Activities/HW.cpp
@@ -2,68 +2,6 @@
 using namespace std;
 
 
-class Queue {
-private:
-    int front;
-    int back;        
-    char items[10]; 
-
-public:
-    Queue() {
-        front = -1;
-        back = -1;
-    }
-
-    ~Queue() {
-    }
-
-    bool isEmpty() {
-        return (front == -1 && back == -1);
-    }
-
-    bool isFull() {
-        return (back == 10 - 1);
-    }
 
 
-    void enQueue(char element) {
-        if (isFull()) {
-            cout << "Queue is full"<< endl;
-            return;
-        }
-        if (isEmpty()) {
-            front = back = 0;
-        } else {
-            back++;
-        }
-        items[back] = element;
-    }
-
-    void deQueue() {
-        if (isEmpty()) {
-            cout << "Queue is empty. Cannot dequeue." << endl
-            return;
-        }
-        if (front == back) {
-            front = back = -1;
-        } else {
-            front++;
-        }
-    }
-
-    char getFront() {
-        if (isEmpty()) {
-            cout << "Queue is empty. No front element." << endl;
-            return '\0'; 
-        }
-        return items[front];
-    }
-
-    char getRear() {
-        if (isEmpty()) {
-            cout << "Queue is empty. No rear element." << endl;
-            return '\0';
-        }
-        return items[back];
-    }
 };
